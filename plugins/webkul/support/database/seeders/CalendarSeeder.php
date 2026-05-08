ba970c96730d3b49eea72f785d278e91e9ceae84
Jitendra <jitendra@webkul.in>
2026-05-08 11:40:28 +0530

    Calendar issue

--- plugins/webkul/support/database/seeders/CalendarSeeder.php
+++ plugins/webkul/support/database/seeders/CalendarSeeder.php
@@ -15,7 +15,17 @@ class CalendarSeeder extends Seeder
         $user = User::first();
 
         DB::table('calendars')->insert([
-            ['creator_id' => $user?->id, 'name' => 'Standard 40 hours/week', 'full_time_required_hours' => 40, 'hours_per_day' => 8, 'flexible_hours' => true, 'timezone' => 'UTC', 'is_active' => true, 'created_at' => now(), 'updated_at' => now()],
+            [
+                'creator_id' => $user?->id,
+                'name' => 'Standard 40 hours/week',
+                'full_time_required_hours' => 40,
+                'hours_per_day' => 8,
+                'flexible_hours' => true,
+                'timezone' => 'UTC',
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
         ]);
     }
 }
